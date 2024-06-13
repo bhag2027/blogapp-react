f6a8b7b503f83dd1388873864a35a464c948925b
bhag2027 <bhagyazz2027@gmail.com>
2024-06-13 14:49:40 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import AddBlog from './components/AddBlog';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
      <BrowserRouter>
     <Routes>
       <Route path='/' element={<AddBlog/>}/>
+      <Route path='/signin' element={<Login/>}/>
       
     </Routes>
     </BrowserRouter>
